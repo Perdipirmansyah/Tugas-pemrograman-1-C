f2c6114a61eadad6995e91ebff4ca4bfd44f2536
Perdipirmansyah <perdipirmansyahh@gmail.com>
2024-09-28 02:43:16 +0700

    Update

---
+++ perdi1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+	int panjang, lebar, luaspersegipanjang;
+	
+	panjang=10;
+	lebar=7;
+	
+	luaspersegipanjang=panjang*lebar;
+	printf ("%d", luaspersegipanjang);
+	
+	return 0;
+}
